b8964097051beb167a6e001f1dafd98897cf264d
Steve Clement <steve@localhost.lu>
2018-05-23 13:39:01 +0200

    - More basic code to extract data.

--- process.py
+++ process.py
@@ -5,15 +5,34 @@ import os
 
 from config import *
 
-with  open('reporterAreas.json') as fCountries:
+with open('reporterAreas.json') as fCountries:
     jCountries = json.load(fCountries)
     fCountries.close()
 
-#print(jCountries)
-print(countries)
+# print(jCountries)
+#print(countries)
 
 cYear = year
 while cYear < end:
     print(str(cYear))
-    pDir = os.path.joint(datadir+year+fmt)
+#    for countryID in countries:
+#        for jID in jCountries['results']:
+#            print(jID[countryID])
+    pDir = os.path.join(datadir + '/' + str(year) + '/' + fmt)
+    noDataDir = os.path.join(datadir + '/' + str(year) + '/' + 'noData')
+    pFiles = os.listdir(pDir)
+    noDataFiles = os.listdir(noDataDir)
+    ##for file in noDataFiles:
+    ##    if file.endswith('csv'):
+    ##        print(str(cYear) + ',' + file)
+    for file in pFiles:
+        pFile=os.path.join(pDir+'/'+file)
+        if file.endswith(fmt):
+            with open(pFile) as f:
+                jFile = json.load(f)
+                if not jFile['dataset']:
+                    print('No data for {} in year {}'.format(file, str(year)))
+                else:
+                    print('We have data for {} in year {}'.format(file, str(year)))
+                f.close()
     cYear += 1
